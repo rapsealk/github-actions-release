d9a8cc40a25fc4c7e5a0783c4b5a0fbb2a344e7d
Jeongseok Kang <piono623@naver.com>
2022-11-21 16:14:28 +0900

    release: 0.0.5 (#2)

--- scripts/build-production.sh
+++ scripts/build-production.sh
@@ -2,15 +2,20 @@
 export RELEASE_VERSION=$(cat ./VERSION)
 echo $RELEASE_VERSION
 
+REDIS_IMAGE=redis:6.2.7-alpine
+
 cp docker-compose.yml docker-compose.prod.yml
-sed -i "s/\${RELEASE_VERSION}/${RELEASE_VERSION}/" docker-compose.prod.yml   #--quiet
+sed -i "s/\${RELEASE_VERSION}/${RELEASE_VERSION}/" docker-compose.prod.yml
 
 docker-compose -f docker-compose.yml build
+
+docker pull --platform linux/amd64 $REDIS_IMAGE
 
 docker save -o "xxxx-${RELEASE_VERSION}-amd64.tar" \
     "xxxx/django:${RELEASE_VERSION}" \
     "xxxx/postgres:${RELEASE_VERSION}" \
-    "xxxx/nginx:${RELEASE_VERSION}"
+    "xxxx/nginx:${RELEASE_VERSION}" \
+    $REDIS_IMAGE
 
 tar -cf xxxx-$RELEASE_VERSION-amd64.release.tar xxxx-$RELEASE_VERSION-amd64.tar docker-compose.prod.yml sample.env
 gzip -9 xxxx-$RELEASE_VERSION-amd64.release.tar
